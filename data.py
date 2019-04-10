ea9b139217213747bbeee8cebd86f14f914ea694
Cameron Jones <cameroncjones4@gmail.com>
2019-04-10 11:58:11 -0700

    Normalize intensities

--- data.py
+++ data.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.signal
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from progress.bar import Bar
 import datetime
 import random
 import math
@@ -29,11 +30,14 @@ WAVE_SIGMA = 7.5 # Standard deviation of wavenumber noise
 INT_SIGMA = 0.2 # Standard deviation of intensity noise
 FWHM = 63.69 # 15cm^-1
 RESOLUTION = 0.1 # Resolution (in wavenumbers) of outputted spectrum
-BUFFER = 0 # Size of 0-padded space between POIs
-POI = () # Points of interest
+BUFFER = 50 # Size of 0-padded space between POIs
+# POI = () # Points of interest
+POI = ((3030.3, 150), (1612.9, 150), (1298.7, 150), (1162.8, 150), (892.9, 150), (787.4, 150), (609.8, 150))
 
 # major emission features at 3.3, 6.2, 7.7, 8.6, 11.2, 12.7, 16.4 μm
+# ((3030.3, 80), (1612.9, 80), (1298.7, 80), (1162.8, 80), (892.9, 80), (787.4, 80), (609.8, 80))
 # weaker features at 3.4, 3.5, 5.25, 5.75, 6.0, 6.9, 7.5, 10.5, 11.0, 13.5, 14.2, 17.4, 18.9 μm
+# ((2941.2, 80), (2857.1, 80), (1904.8, 80), (1739.1, 80), (1666.7, 80), (1449.3, 80), (1333.3, 80), (952.4, 80), (909.1, 80), (740.7, 80), (704.2, 80), (574.7, 80), (529.1, 80))
 
 @click.command()
 @click.option('--input', '-i', default=DEFAULT_DB, help='PAHdb JSON input filename.')
@@ -49,9 +53,10 @@ POI = () # Points of interest
 @click.option('--resolution', default=RESOLUTION, help='Resolution of wavenumber values.')
 @click.option('--buffer', default=BUFFER, help='0-padded buffer betweeen POIs.')
 @click.option('--poi', '-p', default=POI, type=(int, int), multiple=True, help='Points of interest and their width in cm^-1.')
+@click.option('--minimize', is_flag=True, help='Subtract the lowest intensity from each POI.')
 @click.option('--no_scale', is_flag=True, help='Do not apply the scaling factor to all wavenumbers.')
 @click.option('--debug', is_flag=True, help='Stop and display spectrum.')
-def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_training, num_testing, wave_sigma, int_sigma, fwhm, resolution, buffer, poi, no_scale, debug):
+def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_training, num_testing, wave_sigma, int_sigma, fwhm, resolution, buffer, poi, minimize, no_scale, debug):
     # Get date, filename, and resolution multiplier
     now = datetime.datetime.now().strftime('%Y-%m-%d_%I-%M%p')
     filename = f'_n{num_species}-m{mix_size}-p{len(poi)}_{now}'
@@ -121,29 +126,30 @@ def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_traini
         stop = min(vector_size - 1, (point[0] + point[1]) * res_multipler)
         print(f'POI #{p_index + 1}: {start / res_multipler}-{stop / res_multipler} ({(stop - start) / res_multipler})')
 
+    # Intermediate matrices
+    spectra = np.zeros((num_training + num_testing, vector_size), dtype=np.float32)
+    labels = np.zeros((num_training + num_testing, len(uids)), dtype=np.float32)
+
     # Training/testing data matrices
     training_x = np.zeros((num_training, data_length), dtype=np.float32)
     training_y = np.zeros((num_training, len(uids)), dtype=np.float32)
     testing_x = np.zeros((num_testing, data_length), dtype=np.float32)
     testing_y = np.zeros((num_testing, len(uids)), dtype=np.float32)
 
-    # Loop through to generate each sample
+    # Loop through to add noise to each sample
     for i in range(num_training + num_testing):
-        index = i if i < num_training else i - num_training
-
         # Pick random spectra
         species = np.random.choice(data, mix_size, replace=False)
-        spectrum = np.zeros((vector_size), dtype=np.float32)
 
-        dataset = np.zeros((data_length), dtype=np.float32)
-        labels = np.zeros((len(uids)), dtype=np.float32)
+        if i == 0:
+            print(f'Training ({num_training})')
+        elif i == num_training:
+            print(f'Testing ({num_testing})')
 
-        if i < num_training:
-            print(f'Training Sample #{i + 1}')
-        else:
-            print(f'Testing Sample #{i - num_training + 1}')
-        print('UIDs in sample: [', ', '.join(map(str, [m['uid'] for m in species])), ']')
-        print()
+        print(f'Sample {str(i + 1 if i < num_training else i - num_training + 1).zfill(len(str(num_training + num_testing)))} ', end='')
+        uids_in_mix = [m['uid'] for m in species]
+        uids_in_mix.sort()
+        print(f"[ {', '.join(map(str, uids_in_mix))} ]")
 
         # Add noise, bin spectral intensity, and generate labels
         for molecule in species:
@@ -151,12 +157,11 @@ def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_traini
             for transition in molecule['transitions']:
                 wavenumber = int(round(np.random.normal(0, wave_sigma) + transition[0], 1) * res_multipler)
                 intensity = (np.random.normal(0, int_sigma) + 1) * transition[1]
-                # spectrum[wavenumber] += intensity / stats['intensity_max']
-                spectrum[wavenumber] += intensity
-            labels[uids.index(uid)] = 1.0
+                spectra[i][wavenumber] += intensity
+            labels[i][uids.index(uid)] = 1.0
 
         if debug:
-            plt.plot(spectrum)
+            plt.plot(spectra[i])
             plt.show()
 
         # Perform a Gaussian convolution on the whole spectrum
@@ -164,25 +169,45 @@ def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_traini
         # spectrum = scipy.signal.convolve(spectrum, kernel, mode='same')
 
         # Perform a Gaussian convolution on the whole spectrum
-        spectrum = scipy.ndimage.filters.gaussian_filter1d(spectrum, fwhm).astype(np.float16)
+        spectra[i] = scipy.ndimage.filters.gaussian_filter1d(spectra[i], fwhm).astype(np.float16)
 
         if debug:
-            plt.plot(spectrum)
+            plt.plot(spectra[i])
             plt.show()
+            break
+
+    # Calculate bounds and normalize intensity values
+    print('Calculating maximum intensity')
+    max_intensity = max([intensity for spectrum in spectra for intensity in spectrum])
+    print('Max Convolved Intensity:', max_intensity)
+    print('Scaling between 0 and 1')
+    spectra = [[intensity / max_intensity for intensity in spectrum] for spectrum in spectra]
+
+    if not debug:
+        bar = Bar('Slicing spectrum', max=num_training + num_testing)
+
+    # Chop spectrum into POIs
+    for i in range(num_training + num_testing):
+        index = i if i < num_training else i - num_training
+
+        if not debug:
+            bar.next()
+
+        # Dataset
+        dataset = np.zeros((data_length), dtype=np.float32)
 
         # Cut spectra into POIs and rejoin with optional buffers
         count = 0
         for p_index, point in enumerate(poi):
             start = max(0, (point[0] - point[1]) * res_multipler)
             stop = min(vector_size - 1, (point[0] + point[1]) * res_multipler)
-            section = spectrum[start:stop]
+            section = spectra[i][start:stop]
+            min_intensity = min(section)
             for s_index, s in enumerate(section):
-                dataset[count] = s
+                dataset[count] = s - min_intensity if minimize else s
                 count += 1
                 if (s_index == len(section) - 1 and p_index != len(poi) - 1):
                     count += buffer
-
-        # normalize again?
 
         if debug:
             plt.plot(dataset)
@@ -192,10 +217,13 @@ def generate_dataset(input, cutoff, blacklist, num_species, mix_size, num_traini
         # Insert final spectrum and labels into dataset matrix
         if i < num_training:
             training_x[index] = dataset
-            training_y[index] = labels
+            training_y[index] = labels[i]
         else:
             testing_x[index] = dataset
-            testing_y[index] = labels
+            testing_y[index] = labels[i]
+
+    if not debug:
+        bar.finish()
 
     # Create dataset directory if it doesn't exist
     if not os.path.exists('datasets'):
